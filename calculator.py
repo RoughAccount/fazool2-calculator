962dfb7967d800b704c5c7647f6cdd44cf5b9073
Abdullah Bajwa <abdullahbajwa2019@gmail.com>
2025-12-29 11:15:30 +0500

    add subtraction process and exception handling

--- calculator.py
+++ calculator.py
@@ -4,7 +4,12 @@ a = int(input("enter a number: "))
 b = int(input("enter the second number: "))
 
 print("to perform addition enter +: ")
+print("to perform subtraction enter -: ")
 
 key = msvcrt.getch()
 if key.decode() == '+':
     print("addition is ",a+b)
+elif key.decode() == '-':
+    print("subtraction is ",a-b)
+else:
+    print("invalid operator (^_^)")
