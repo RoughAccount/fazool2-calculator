4a6a5c39b7daa88e9a198c87019b4c7fa05a7a00
Abdullah Bajwa <abdullahbajwa2019@gmail.com>
2025-12-29 11:17:54 +0500

    add multiplication

--- calculator.py
+++ calculator.py
@@ -5,11 +5,14 @@ b = int(input("enter the second number: "))
 
 print("to perform addition enter +: ")
 print("to perform subtraction enter -: ")
+print("to perform multiplication enter *: ")
 
 key = msvcrt.getch()
 if key.decode() == '+':
     print("addition is ",a+b)
 elif key.decode() == '-':
     print("subtraction is ",a-b)
+elif key.decode() == '*':
+    print("multiplied is ",a*b)
 else:
     print("invalid operator (^_^)")
